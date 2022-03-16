86c77b53c9c10e9e50661c09dbba8473f1e08167
Trong Tan <e0550425@u.nus.edu>
2022-03-16 15:39:38 +0800

    refactor rendering system

--- CloudJumpers/GameEngine/GameSystem/Rendering/RenderingSystem.swift
+++ CloudJumpers/GameEngine/GameSystem/Rendering/RenderingSystem.swift
@@ -31,8 +31,7 @@ class RenderingSystem: System {
             else {
                 return
             }
-//            let newNode = SKNodeFactory.createSKSpriteNode(type: component.type)
-            let newNode = SKNodeFactory.createSKSpriteNode(renderingComponent: component)
+            let newNode = RenderingFactory.createSKSpriteNode(renderingComponent: component)
 
             entitiesManager?.addNode(newNode, entity: entity)
             component.isUpdating = false
