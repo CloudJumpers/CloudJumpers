6683e9581744abdfb4deb04fe0e3e37ab5a30ce3
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2022-03-25 13:19:26 +0800

    feat: Added new Platform entity

---
+++ CloudJumpers/EntitySystem/Entities/Platform.swift
@@ -0,0 +1,47 @@
+//
+//  Platform.swift
+//  CloudJumpers
+//
+//  Created by Phillmont Muktar on 23/3/22.
+//
+
+import SpriteKit
+
+class Platform: Entity {
+    let id: ID
+
+    private let position: CGPoint
+
+    init(at position: CGPoint) {
+        id = UUID()
+        self.position = position
+    }
+
+    func setUpAndAdd(to manager: EntityManager) {
+        let spriteComponent = createSpriteComponent()
+        let physicsComponent = createPhysicsComponent(for: spriteComponent)
+
+        manager.addComponent(spriteComponent, to: self)
+        manager.addComponent(physicsComponent, to: self)
+    }
+
+    private func createSpriteComponent() -> SpriteComponent {
+        // TODO: Abstract out Clouds texture atlas
+        let texture = SKTextureAtlas(named: "Clouds").textureNamed("cloud-1")
+        let spriteComponent = SpriteComponent(texture: texture, size: Constants.platformNodeSize, at: position)
+        spriteComponent.node.zPosition = SpriteZPosition.platform.rawValue
+
+        return spriteComponent
+    }
+
+    private func createPhysicsComponent(for spriteComponent: SpriteComponent) -> PhysicsComponent {
+        let physicsComponent = PhysicsComponent(rectangleOf: Constants.platformPhysicsSize, for: spriteComponent)
+        physicsComponent.body.affectedByGravity = false
+        physicsComponent.body.allowsRotation = false
+        physicsComponent.body.isDynamic = false
+        physicsComponent.body.categoryBitMask = Constants.bitmaskPlatform
+        physicsComponent.body.contactTestBitMask = Constants.bitmaskPlayer
+
+        return physicsComponent
+    }
+}
