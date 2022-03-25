3a57344e26e1f794e9540d85b61ffe83d71e16b9
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2022-03-25 13:24:30 +0800

    feat: New Input management system

---
+++ CloudJumpers/Input/InputResponder.swift
@@ -0,0 +1,22 @@
+//
+//  InputResponder.swift
+//  CloudJumpers
+//
+//  Created by Phillmont Muktar on 23/3/22.
+//
+
+import SpriteKit
+
+protocol InputResponder: AnyObject {
+    var associatedEntity: Entity? { get set }
+
+    func inputMove(by displacement: CGVector)
+    func inputJump()
+    func inputPause()
+}
+
+extension InputResponder {
+    func inputMove(by displacement: CGVector) { }
+    func inputJump() { }
+    func inputPause() { }
+}
