cdceeeea2035938a971f9af4cbbe0e3b0d3c297c
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2022-03-25 23:40:26 +0800

    feat: Added new MoveEvent and JumpEvent

---
+++ CloudJumpers/Events/MoveEvent.swift
@@ -0,0 +1,30 @@
+//
+//  MoveEvent.swift
+//  CloudJumpers
+//
+//  Created by Phillmont Muktar on 25/3/22.
+//
+
+import Foundation
+import CoreGraphics
+
+struct MoveEvent: Event {
+    let timestamp: TimeInterval
+
+    private let entityID: EntityID
+    private let displacement: CGVector
+
+    init(on entity: Entity, by displacement: CGVector) {
+        timestamp = EventManager.timestamp
+        entityID = entity.id
+        self.displacement = displacement
+    }
+
+    func execute(in entityManager: EntityManager) {
+        guard let entity = entityManager.entity(with: entityID),
+              let spriteComponent = entityManager.component(ofType: SpriteComponent.self, of: entity)
+        else { return }
+
+        spriteComponent.node.position += CGVector(dx: displacement.dx, dy: 0)
+    }
+}
