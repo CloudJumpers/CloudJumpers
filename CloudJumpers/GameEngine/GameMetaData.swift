a8ed47b1cdb57464fc20f32167fa8796b9b38c57
Hoang Trong Tan <67775223+jushg@users.noreply.github.com>
2022-03-27 01:40:28 +0800

    Update CloudJumpers/GameEngine/GameMetaData.swift
    Co-authored-by: Sujay R Subramanian <40201586+rssujay@users.noreply.github.com>

--- CloudJumpers/GameEngine/GameMetaData.swift
+++ CloudJumpers/GameEngine/GameMetaData.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class GameMetaData {
-    var time: Double = 0
+    var time: Double = Double.zero
     var playerId = EntityID()
     var playerLocationMapping = [EntityID: EntityID]()
     var topPlatformId = EntityID()
