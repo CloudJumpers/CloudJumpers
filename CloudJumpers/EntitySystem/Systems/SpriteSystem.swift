0a66be23e3fac2151b10826e709a9317bc90002c
Eric Bryan <e0555789@u.nus.edu>
2022-03-27 11:34:17 +0800

    fix linting error

--- CloudJumpers/EntitySystem/Systems/SpriteSystem.swift
+++ CloudJumpers/EntitySystem/Systems/SpriteSystem.swift
@@ -45,12 +45,12 @@ class SpriteSystem: System {
                   let timedComponent = manager.component(ofType: TimedComponent.self, of: entity)  else {
                 continue
             }
-            
+
             // animation to fade power-up effect
             let node = spriteComponent.node
             let time = timedComponent.time
             node.alpha = (Constants.powerUpEffectDuration - time) / Constants.powerUpEffectDuration
-            
+
             if powerUpEffect.shouldRemoveEffect(manager: manager) {
                 removeNodeFromScene(entity)
                 manager.remove(entity)
