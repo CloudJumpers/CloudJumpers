36868063e858d534cdf567d5a60199588ac35798
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2022-03-29 15:22:15 +0800

    refactor: SinglePlayerGameEngine is now GameEngine

--- CloudJumpers/GameEngine/GameEngine.swift
+++ CloudJumpers/GameEngine/GameEngine.swift
@@ -1,21 +1,167 @@
 //
 //  GameEngine.swift
-//  Cloud Jumpers
+//  CloudJumpers
 //
-//  Created by Trong Tan on 3/8/22.
+//  Created by Phillmont Muktar on 24/3/22.
 //
 
 import SpriteKit
 
-protocol GameEngine: InputResponder {
-    var entityManager: EntityManager { get }
-    var eventManager: EventManager { get }
-    var contactResolver: ContactResolver { get }
-    var delegate: GameEngineDelegate? { get }
-    var metaData: GameMetaData { get set }
-    var systems: [System] { get set }
+class GameEngine {
+    let entityManager: EntityManager
+    let eventManager: EventManager
+    let contactResolver: ContactResolver
+    weak var delegate: GameEngineDelegate?
+    var systems: [System]
+    var associatedEntity: Entity?
+    var metaData: GameMetaData
 
-    init(for delegate: GameEngineDelegate, channel: NetworkID?)
-    func setUpGame()
-    func update(within time: CGFloat)
+    private var crossDeviceSyncTimer: Timer?
+
+    required init(for delegate: GameEngineDelegate, channel: NetworkID? = nil) {
+        metaData = GameMetaData()
+        entityManager = EntityManager()
+        eventManager = EventManager(channel: channel)
+        contactResolver = ContactResolver(to: eventManager)
+        systems = []
+        self.delegate = delegate
+        contactResolver.metaDataDelegate = self
+        setUpSystems()
+        setUpCrossDeviceSyncTimer()
+    }
+
+    deinit {
+        crossDeviceSyncTimer?.invalidate()
+    }
+
+    func update(within time: CGFloat) {
+        updateEvents()
+        updateTime()
+        updateSystems(within: time)
+    }
+
+    func setUpGame(with entities: [Entity]) {
+        entities.forEach { entity in
+            entityManager.add(entity)
+            addNodeToScene(entity, with: delegate?.engine(_:addEntityWith:))
+        }
+
+        setUpSampleGame()
+    }
+
+    private func setUpCrossDeviceSyncTimer() {
+        crossDeviceSyncTimer = Timer.scheduledTimer(
+            withTimeInterval: GameConstants.positionalUpdateIntervalSeconds,
+            repeats: true
+        ) { [weak self] _ in self?.syncToOtherDevices() }
+    }
+
+    private func setUpSystems() {
+        systems.append(TimedSystem(for: entityManager))
+        systems.append(SpriteSystem(for: entityManager))
+    }
+
+    private func updateSystems(within time: CGFloat) {
+        for system in systems {
+            system.update(within: time)
+        }
+    }
+
+    // MARK: - Temporary methods to abstract
+    private var timer: TimedLabel?
+
+    private func setUpSampleGame() {
+        guard let userId = AuthService().getUserId() else {
+            return
+        }
+
+        let timer = TimedLabel(at: Constants.timerPosition, initial: Constants.timerInitial)
+        let player = Player(at: Constants.playerInitialPosition, texture: .character1, with: userId)
+        let topPlatform = Platform(at: CGPoint(x: 0, y: 700))
+
+        entityManager.add(timer)
+        entityManager.add(player)
+        entityManager.add(topPlatform)
+
+        addNodeToScene(timer, with: delegate?.engine(_:addControlWith:))
+        addNodeToScene(player, with: delegate?.engine(_:addPlayerWith:))
+        addNodeToScene(topPlatform, with: delegate?.engine(_:addEntityWith:))
+
+        self.timer = timer
+        associatedEntity = player
+        metaData.playerId = player.id
+        metaData.topPlatformId = topPlatform.id
+    }
+
+    private func addNodeToScene(_ entity: Entity, with method: ((GameEngine, SKNode) -> Void)?) {
+        guard let spriteComponent = entityManager.component(ofType: SpriteComponent.self, of: entity) else {
+            return
+        }
+
+        method?(self, spriteComponent.node)
+    }
+
+    private func updateEvents() {
+        eventManager.executeAll(in: entityManager)
+    }
+
+    // MARK: Temporary time update method
+    private func updateTime() {
+        guard let timer = timer,
+              let timedComponent = entityManager.component(ofType: TimedComponent.self, of: timer)
+        else { return }
+
+        metaData.time = timedComponent.time
+    }
+}
+
+// MARK: - GameMetaDataDelegate
+extension GameEngine: GameMetaDataDelegate {
+    func updatePlayerPosition(position: CGPoint) {
+        metaData.playerPosition = position
+    }
+
+    func updatePlayerTextureKind(texture: Textures.Kind) {
+        metaData.playerTexture = texture
+    }
+
+    func metaData(changePlayerLocation player: EntityID, location: EntityID?) {
+        if let location = location {
+            metaData.playerLocationMapping[player] = location
+        } else {
+            metaData.playerLocationMapping.removeValue(forKey: player)
+        }
+    }
+
+    func syncToOtherDevices() {
+        let positionalUpdate = OnlineRepositionEvent(
+            positionX: metaData.playerPosition.x,
+            positionY: metaData.playerPosition.y,
+            texture: metaData.playerTexture.rawValue
+        )
+
+        let repositionCmd = RepositionEventCommand(sourceId: metaData.playerId, event: positionalUpdate)
+        eventManager.dispatchGameEventCommand(repositionCmd)
+    }
+}
+
+// MARK: - InputResponder
+extension GameEngine: InputResponder {
+    func inputMove(by displacement: CGVector) {
+        guard let entity = associatedEntity else {
+            return
+        }
+
+        var event = MoveEvent(on: entity, by: displacement)
+        event.gameDataTracker = self
+        eventManager.add(event)
+    }
+
+    func inputJump() {
+        guard let entity = associatedEntity else {
+            return
+        }
+
+        eventManager.add(JumpEvent(on: entity))
+    }
 }
