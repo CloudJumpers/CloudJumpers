074c2557ece70180780ccc7f5a8f4093f7981685
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2022-03-30 15:50:04 +0800

    feat: Added SoundManager

---
+++ CloudJumpers/Sounds/SoundManager.swift
@@ -0,0 +1,38 @@
+//
+//  SoundManager.swift
+//  CloudJumpers
+//
+//  Created by Phillmont Muktar on 29/3/22.
+//
+
+import AVFAudio
+
+class SoundManager {
+    static let i = SoundManager()
+    private var players: [Sounds: AVAudioPlayer]
+
+    init() {
+        players = [:]
+    }
+
+    func play(_ sound: Sounds, loopsBy loops: Int = 0) {
+        if players[sound] == nil {
+            players[sound] = sound.player
+        }
+
+        players[sound]?.numberOfLoops = loops
+        players[sound]?.play()
+    }
+
+    func pause(_ sound: Sounds) {
+        players[sound]?.pause()
+    }
+
+    func stop(_ sound: Sounds) {
+        players[sound]?.stop()
+    }
+
+    func stopAll() {
+        players.values.forEach { $0.stop() }
+    }
+}
