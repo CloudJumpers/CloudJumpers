9a0b885dca830a85497bf7d0a6037b75f78c4bc1
rssujay <rssujay97@gmail.com>
2022-04-06 22:09:59 +0800

    Add comment

--- CloudJumpers/Networking/Game/Subscribers/FirebaseSubscriber.swift
+++ CloudJumpers/Networking/Game/Subscribers/FirebaseSubscriber.swift
@@ -1,5 +1,5 @@
 //
-//  RealtimeSubscriber.swift
+//  FirebaseSubscriber.swift
 //  CloudJumpers
 //
 //  Created by Sujay R Subramanian on 24/3/22.
--- CloudJumpers/Networking/Lobbies/Updaters/FirebaseUpdaterDelegate.swift
+++ CloudJumpers/Networking/Lobbies/Updaters/FirebaseUpdaterDelegate.swift
@@ -90,6 +90,9 @@ class FirebaseUpdaterDelegate: LobbyUpdaterDelegate {
             let lobbyReference = getLobbyReference(lobbyId: lobby.id)
             let channelReference = getLobbyChannelReference(lobbyId: lobby.id)
             lobbyReference.removeValue()
+            // Perform a deferred delete of channel.
+            // Provides some grace period for all updates across devices
+            // to reach the server first.
             channelReference.onDisconnectRemoveValue()
         } else {
             let userReference = getLobbyUserReference(lobbyId: lobby.id, userId: userId)
