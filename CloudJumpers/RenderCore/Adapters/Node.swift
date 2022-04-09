c3526a70c4e3f37d45cd0d02c6352da8c70ef047
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2022-04-09 15:48:15 +0800

    feat: Added new facade Node

---
+++ CloudJumpers/RenderCore/Adapters/Node.swift
@@ -0,0 +1,85 @@
+//
+//  Node.swift
+//  CloudJumpers
+//
+//  Created by Phillmont Muktar on 6/4/22.
+//
+
+import SpriteKit
+
+typealias NodeCore = SKNode
+
+class Node {
+    private var captionNodeCore: SKLabelNode?
+
+    let nodeCore: NodeCore
+    var name: String?
+
+    var physicsBody: PhysicsBody? {
+        didSet {
+            nodeCore.physicsBody = physicsBody?.bodyCore
+        }
+    }
+
+    init(texture: TextureFrame, size: CGSize) {
+        nodeCore = SKSpriteNode(texture: Texture.texture(of: texture), size: size)
+    }
+
+    var position: CGPoint {
+        get { nodeCore.position }
+        set { nodeCore.position = newValue }
+    }
+
+    var zPosition: CGFloat {
+        get { nodeCore.zPosition }
+        set { nodeCore.zPosition = newValue }
+    }
+
+    var alpha: CGFloat {
+        get { nodeCore.alpha }
+        set { nodeCore.alpha = newValue }
+    }
+
+    func scale(by scale: CGVector) {
+        nodeCore.xScale = scale.dx
+        nodeCore.yScale = scale.dy
+    }
+
+    func addChild(_ node: Node) {
+        nodeCore.addChild(node.nodeCore)
+    }
+
+    func caption(with label: String) {
+        // TODO: Style this caption up
+        let captionNodeCore = SKLabelNode(text: label)
+        nodeCore.addChild(captionNodeCore)
+        self.captionNodeCore = captionNodeCore
+    }
+
+    func move(to position: CGPoint, within duration: TimeInterval) {
+        nodeCore.run(.move(to: position, duration: duration))
+    }
+
+    func move(by displacement: CGVector, within duration: TimeInterval) {
+        nodeCore.run(.move(by: displacement, duration: duration))
+    }
+
+    func animate(with textures: [TextureFrame], interval: TimeInterval) {
+        nodeCore.run(.animate(
+            with: Texture.textures(of: textures),
+            timePerFrame: interval,
+            resize: false,
+            restore: true))
+    }
+}
+
+// MARK: - Hashable
+extension Node: Hashable {
+    static func == (lhs: Node, rhs: Node) -> Bool {
+        lhs.nodeCore == rhs.nodeCore
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(nodeCore)
+    }
+}
