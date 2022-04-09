ce51d14b2d31debeee13568cb96c5def764f30ae
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2022-04-09 15:48:59 +0800

    feat: Added PhysicsCategory (WIP)

---
+++ CloudJumpers/RenderCore/PhysicsCategory.swift
@@ -0,0 +1,13 @@
+//
+//  PhysicsCategory.swift
+//  CloudJumpers
+//
+//  Created by Phillmont Muktar on 9/4/22.
+//
+
+import Foundation
+
+enum PhysicsCategory: UInt32 {
+    case none = 0
+    case max = 0xFFFFFFFF
+}
