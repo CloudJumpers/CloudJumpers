7366f5eb8f3d578da812091d3411a7c679c55a0c
Eric Bryan <e0555789@u.nus.edu>
2022-04-10 14:30:53 +0800

    Added PhyscisCollision and PhysicsContactTest

--- CloudJumpers/RenderCore/PhysicsCategory.swift
+++ CloudJumpers/RenderCore/PhysicsCategory.swift
@@ -9,5 +9,13 @@ import Foundation
 
 enum PhysicsCategory: UInt32 {
     case none = 0
+    case player = 0x00000001
+    case cloud = 0x00000002
+    case platform = 0x00000004
+    case powerUp = 0x00000008
+    case disaster = 0x00000010
+    case wall = 0x00000020
+    case floor = 0x00000040
+    case guest = 0x00000080
     case max = 0xFFFFFFFF
 }
