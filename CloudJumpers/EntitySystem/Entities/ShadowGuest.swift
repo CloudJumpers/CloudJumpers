5d36e97b8b8691d244de2eb81983e1038ef62367
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2022-04-14 22:45:30 +0800

    feat: ShadowGuest now uses texture

--- CloudJumpers/EntitySystem/Entities/ShadowGuest.swift
+++ CloudJumpers/EntitySystem/Entities/ShadowGuest.swift
@@ -14,14 +14,9 @@ class ShadowGuest: Entity {
 
     private(set) var position: CGPoint
     private let name: String
-    private let texture: Textures
+    private let texture: Characters
 
-    init(
-        at position: CGPoint,
-        texture: Textures,
-        name: String,
-        with id: EntityID = EntityManager.newEntityID
-    ) {
+    init(at position: CGPoint, texture: Characters, name: String, with id: EntityID = EntityManager.newEntityID) {
         self.id = id
         self.texture = texture
         self.name = name
@@ -39,32 +34,7 @@ class ShadowGuest: Entity {
     }
 
     private func createSpriteComponent() -> SpriteComponent {
-        let spriteComponent = SpriteComponent(
-            texture: texture.idle,
-            size: Constants.playerSize,
-            at: position,
-            forEntityWith: id)
-
-        spriteComponent.node.zPosition = SpriteZPosition.shadowGuest.rawValue
-        createNameLabel(for: spriteComponent)
-
-        return spriteComponent
-    }
-
-    private func createNameLabel(for spriteComponent: SpriteComponent) {
-        var displayname = name
-        if displayname.count > Constants.playerDisplaynameSize {
-            let index = displayname.index(displayname.startIndex, offsetBy: Constants.playerDisplaynameSize)
-            displayname = displayname[..<index] + "..."
-        }
-
-        let labelNode = SKLabelNode()
-        labelNode.text = displayname
-        labelNode.fontSize = Constants.captionFontSize
-        labelNode.position = Constants.captionRelativePosition
-        labelNode.fontColor = .black
-
-        spriteComponent.node.addChild(labelNode)
+        SpriteComponent(texture: texture.idle, size: Constants.playerSize, zPosition: .shadowGuest)
     }
 
     private func createPhysicsComponent(for spriteComponent: SpriteComponent) -> PhysicsComponent {
