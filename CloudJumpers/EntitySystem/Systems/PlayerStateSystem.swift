4feb96d73db86d2fa18d8e4dd42bf0902d3f4c80
Trong Tan <e0550425@u.nus.edu>
2022-04-16 16:26:30 +0800

    fix: Player State System

--- CloudJumpers/EntitySystem/Systems/PlayerStateSystem.swift
+++ CloudJumpers/EntitySystem/Systems/PlayerStateSystem.swift
@@ -14,35 +14,27 @@ class PlayerStateSystem: System {
     unowned var manager: EntityManager?
     unowned var dispatcher: EventDispatcher?
 
-    private var crossDeviceSyncTimer: Timer?
+    private var updateCounter: Int = .zero
 
     required init(for manager: EntityManager) {
         self.manager = manager
-        setUpCrossDeviceSyncTimer()
-    }
-
-    deinit {
-        crossDeviceSyncTimer?.invalidate()
     }
 
     func update(within time: CGFloat) {
+        updateCounter += 1
+        if updateCounter == 6 {
+            uploadLocalPlayerState()
+            updateCounter = 0
         }
-
-    private func setUpCrossDeviceSyncTimer() {
-        crossDeviceSyncTimer = Timer.scheduledTimer(
-            withTimeInterval: GameConstants.positionalUpdateIntervalSeconds,
-            repeats: true
-        ) { [weak self] _ in self?.uploadLocalPlayerState() }
     }
 
     func uploadLocalPlayerState() {
-        guard let playerEntity = manager?.components(ofType: PlayerTag.self).first?.entity,
+        guard let playerEntity = getPlayerEntity(),
               let positionComponent = manager?.component(ofType: PositionComponent.self, of: playerEntity),
               let animationComponent = manager?.component(ofType: AnimationComponent.self, of: playerEntity),
               let animation = animationComponent.activeAnimation
         else { return }
 
-        // TODO: Change after new way of getting sprite texture
         let playerPosition = positionComponent.position
         let playerTexture = animation.key
 
@@ -57,6 +49,5 @@ class PlayerStateSystem: System {
 
     func getPlayerEntity() -> Entity? {
         manager?.components(ofType: PlayerTag.self).first?.entity
-
     }
 }
