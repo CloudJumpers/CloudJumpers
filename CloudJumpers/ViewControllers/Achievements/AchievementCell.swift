effc9283e8f8f1deed2587d0c0110bf8fdbf9749
rssujay <rssujay97@gmail.com>
2022-04-16 18:46:55 +0800

    Change opacity affector

--- CloudJumpers/ViewControllers/Achievements/AchievementCell.swift
+++ CloudJumpers/ViewControllers/Achievements/AchievementCell.swift
@@ -38,7 +38,7 @@ class AchievementCell: UICollectionViewCell {
     }
 
     func setUnlockStatus(_ isUnlocked: Bool) {
-        self.layer.opacity = isUnlocked ? 1.0 : 0.5
+        achievmentImage.layer.opacity = isUnlocked ? 1.0 : 0.25
     }
 
     func setRequired(_ required: String) {
