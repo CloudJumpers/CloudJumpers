1996f1f414525e53a9ceee69909e5724f26a85f8
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2022-04-16 22:24:01 +0800

    fix(MetricsSystem): Updated to conform new EventDispatcher specs

--- CloudJumpers/EntitySystem/Systems/MetricsSystem.swift
+++ CloudJumpers/EntitySystem/Systems/MetricsSystem.swift
@@ -16,7 +16,7 @@ class MetricsSystem: System {
     unowned var manager: EntityManager?
     unowned var dispatcher: EventDispatcher?
 
-    required init(for manager: EntityManager) {
+    required init(for manager: EntityManager, dispatchesVia dispatcher: EventDispatcher? = nil) {
         self.manager = manager
         self.counts = [:]
     }
