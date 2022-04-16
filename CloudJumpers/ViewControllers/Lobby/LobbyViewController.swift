6f6a8b3738d99001e3e3e230375ab7aacd77df1e
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2022-04-16 14:37:51 +0800

    fix(LobbyVC): Breaking constraints with height = 145

--- CloudJumpers/ViewControllers/Lobby/LobbyViewController.swift
+++ CloudJumpers/ViewControllers/Lobby/LobbyViewController.swift
@@ -20,6 +20,7 @@ class LobbyViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        lobbyUsersView.delegate = self
         lobbyUsersView.dataSource = self
         gameSeed.delegate = self
         navigationItem.hidesBackButton = true
@@ -200,19 +201,22 @@ extension LobbyViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: LobbyConstants.LobbyUserCellIdentifier, for: indexPath)
 
-        guard
-            let lobbyUser = activeLobby?.orderedValidUsers[indexPath.row],
-            let lobbyUserCell = cell as? LobbyUserCell,
-            let lobbyHostId = activeLobby?.hostId
-        else {
-            return cell
-        }
+        guard let lobbyUser = activeLobby?.orderedValidUsers[indexPath.row],
+              let lobbyUserCell = cell as? LobbyUserCell,
+              let lobbyHostId = activeLobby?.hostId
+        else { return cell }
 
         lobbyUserCell.setIsHostLabelVisible(isVisible: lobbyUser.id == lobbyHostId)
         lobbyUserCell.setDisplayName(newDisplayName: lobbyUser.displayName)
         lobbyUserCell.setIsReady(isReady: lobbyUser.isReady)
 
         return cell
+    }
+}
+
+extension LobbyViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        145.0
     }
 }
 
