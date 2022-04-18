5eb4181f5c982c2e4ff6c8365bec2163d5240a8d
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2022-04-18 18:00:50 +0800

    feat: LabelComponent supports custom fonts

--- CloudJumpers/EntitySystem/Entities/StaticLabel.swift
+++ CloudJumpers/EntitySystem/Entities/StaticLabel.swift
@@ -5,26 +5,43 @@
 //  Created by Trong Tan on 4/15/22.
 //
 
-import Foundation
-import SpriteKit
+import UIKit
 
 class StaticLabel: Entity {
     let id: EntityID
 
     private var position: CGPoint
-    private var fontSize: CGFloat
+    private var typeface: Fonts
+    private var size: CGFloat
     private var text: String
+    private var color: UIColor
 
-    init(at position: CGPoint, fontSize: CGFloat, text: String, with id: EntityID = EntityManager.newEntityID) {
+    init(
+        at position: CGPoint,
+        typeface: Fonts,
+        size: CGFloat,
+        text: String,
+        color: UIColor = .black,
+        with id: EntityID = EntityManager.newEntityID
+    ) {
         self.id = id
         self.position = position
-        self.fontSize = fontSize
+        self.typeface = typeface
+        self.size = size
+        self.color = color
         self.text = text
     }
 
     func setUpAndAdd(to manager: EntityManager) {
+        manager.addComponent(createLabelComponent(), to: self)
         manager.addComponent(PositionComponent(at: position), to: self)
-        manager.addComponent(LabelComponent(text: text, fontSize: fontSize), to: self)
         manager.addComponent(CameraStaticTag(), to: self)
+    }
+
+    private func createLabelComponent() -> LabelComponent {
+        let labelComponent = LabelComponent(text: text, size: size)
+        labelComponent.typeface = typeface
+        labelComponent.color = color
+        return labelComponent
     }
 }
