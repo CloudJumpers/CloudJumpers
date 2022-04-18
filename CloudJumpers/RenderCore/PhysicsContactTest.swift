efda42e9953f28ca08f8951097b9d46c7bf039b8
Eric Bryan <e0555789@u.nus.edu>
2022-04-18 16:05:18 +0800

    cloud and guest contact

--- CloudJumpers/RenderCore/PhysicsContactTest.swift
+++ CloudJumpers/RenderCore/PhysicsContactTest.swift
@@ -12,13 +12,13 @@ struct PhysicsContactTest {
     static let player: PhysicsBitMask = PhysicsCategory.max ^ PhysicsCategory.guest
     static let cloud: PhysicsBitMask = PhysicsCategory.disaster | PhysicsCategory.player |
     PhysicsCategory.guest | PhysicsCategory.wall
-    static let platform: PhysicsBitMask = PhysicsCategory.disaster | PhysicsCategory.player
+    static let platform: PhysicsBitMask = PhysicsCategory.disaster | PhysicsCategory.player | PhysicsCategory.guest
     static let powerUp: PhysicsBitMask = PhysicsCategory.player
     static let disaster: PhysicsBitMask = PhysicsCategory.cloud | PhysicsCategory.player |
     PhysicsCategory.platform | PhysicsCategory.floor
     static let wall: PhysicsBitMask = PhysicsCategory.cloud
     static let floor: PhysicsBitMask = PhysicsCategory.disaster
-    static let guest: PhysicsBitMask = PhysicsCategory.wall | PhysicsCategory.floor | PhysicsCategory.cloud
+    static let guest: PhysicsBitMask = PhysicsCategory.wall | PhysicsCategory.floor | PhysicsCategory.cloud | PhysicsCategory.platform
     static let shadowGuest: PhysicsBitMask = PhysicsCategory.wall | PhysicsCategory.floor
     static let max: PhysicsBitMask = 0xFFFFFFFF
 }
