4249ed7ef62ff5eb03a2a404f15037dba58160d1
Eric Bryan <e0555789@u.nus.edu>
2022-04-19 09:57:42 +0800

    Disaster factory

---
+++ CloudJumpers/Events/DisasterEvents/MeteorHitEventCreator.swift
@@ -0,0 +1,20 @@
+//
+//  MeteorEventCreator.swift
+//  CloudJumpers
+//
+//  Created by Eric Bryan on 17/4/22.
+//
+
+import Foundation
+
+class MeteorHitEventCreator: DisasterHitEventCreator {
+    required init() {}
+
+    func create(from disasterId: EntityID, on entityID: EntityID) -> Event {
+        DisasterPlayerCollideEvent(from: disasterId, on: entityID)
+    }
+
+    func doesMatch(type: DisasterComponent.Kind) -> Bool {
+        type == .meteor
+    }
+}
